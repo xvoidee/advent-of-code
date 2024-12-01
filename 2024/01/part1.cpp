ebc7472ef2d7a0e7c8d6e33238e94291b688c331
xvoidee <alexander.chaika@gmail.com>
2024-12-01 23:28:32 +0100

    Solution to day 1

---
+++ 2024/01/part1.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::ifstream f("input.txt");
+
+	int x, y;
+
+	std::vector<int> a, b;
+
+	while (f >> x >> y) {
+		a.push_back(x);
+		b.push_back(y);
+	}
+
+	std::sort(a.begin(), a.end());
+	std::sort(b.begin(), b.end());
+
+	int sum = 0;
+
+	for (int i = 0; i < a.size(); ++i) {
+		sum += std::abs(a[i] - b[i]);
+	}
+
+	std::cout << sum << '\n';
+
+	return 0;
+}
+
---
+++ 2024/01/part2.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+
+int main()
+{
+	std::ifstream f("input.txt");
+
+	int x, y;
+
+	std::vector<int> a, b;
+
+	while (f >> x >> y) {
+		a.push_back(x);
+		b.push_back(y);
+	}
+
+	auto count = [&b] (int v) -> int {
+		int amount = 0;
+
+		for (auto i : b) {
+			if (i == v) {
+				amount++;
+			}
+		}
+
+		return amount;
+	};
+
+	int result = 0;
+	std::map<int, int> cache;
+	for (auto i : a) {
+		if (cache.find(i) == cache.end()) {
+			cache[i] = count(i);
+		}
+
+		result += i * cache[i];
+	}
+
+	std::cout << result << '\n';
+
+	return 0;
+}
+
